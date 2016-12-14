5cd64eaec48837ca61847c821c2b6f333cc0fef5
Jakub Adamczyk <adamczyk777@gmial.com>
2016-12-14 18:15:16 +0100

    initial commit

---
+++ dynamicData.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void read(char *strings[], int i){
+    char buffer[100];
+    fgets(buffer, 100, stdin);
+    strings[i] = (char*)malloc((strlen(buffer)+1)*sizeof(char));
+    strcpy(strings[i], buffer);
+}
+void main(){
+    char *strings[5];
+    int i;
+    for(i=0; i<5; i++){
+        read(strings, i);
+    }
+}
