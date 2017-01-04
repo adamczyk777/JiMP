75960b0696f1a03475a06fbc96ccc1e976ea7acd
Jakub Adamczyk <adamjaku@charon.kis.agh.edu.pl>
2017-01-04 18:28:19 +0100

    add working code!!

--- books.c
+++ books.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 #include <math.h>
 
-void main(void){
-    int quantity;
-    scanf("%d", &quantity);
-    struct book{
-        char nameSurname[];
-        char title[];
+struct book {
+        char nameSurname[100];
+        char title[100];
         int catNo;
-        float price;
+        double price;
     };
-    struct book books[quantity];
+
+void main(void){
+    int quantity, i;
+    printf("wpisz liczbe pozycji w tablicy: \n");
+    scanf("%d", &quantity);
+    struct book* booksArray;
+    booksArray = malloc(sizeof(booksArray) * quantity);
+    for(i = 0; i < quantity; i++){
+        scanf("%s", booksArray[i].nameSurname);
+        scanf("%s", booksArray[i].title);
+        scanf("%d", &booksArray[i].catNo);
+        scanf("%lf", &booksArray[i].price);
+    }
 }
