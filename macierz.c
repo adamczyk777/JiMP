8145118362d6506e2d43ec05ce485df08ad03a55
Jakub Adamczyk <adamczyk777@gmail.com>
2017-01-14 21:51:56 +0100

    add exam files

---
+++ macierz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* Jakub Adamczyk Zadanie Kolokwium */
+
+struct element{
+    double value;
+    int x;
+    int y;
+};
+
+void analiza_elementow()
+
+int main(void){
+    struct element struktury[25];
+    int macierz[5][5] = {{1,0,0,0,0}, {0,1,0,0,0}, {0,0,1,0,0,}, {0,0,0,1,0}, {0,0,0,0,1}};
+    int r, n, m;
+    for(n = 0; n < sizeof(macierz)/sizeof(macierz[0]); n++){
+        for(m = 0; m < sizeof(macierz[0])/sizeof(macierz[0][0]);m++){
+
+        }
+    }
+    return 0;
+}
